f76b3e0c2731ed1d9871b85fa631e8c8efe0b322
Alexander Murray-Watters <amurrayw@cmu.edu>
2013-02-14 19:53:36 -0500

    Update noisemaker.py

--- noisemaker.py
+++ noisemaker.py
@@ -12,38 +12,44 @@ import mechanize
 import cookielib
 
 # Browser
-br = mechanize.Browser()
+browse = mechanize.Browser()
 
 # Dig into what these lines do. Clearly they invovle instantiating a cookie object. The characteristics of this object, however, are presently unknown.
 # Cookie Jar
 cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+browse.set_cookiejar(cj)
 
 
-# Dig into what these lines do.
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+
+# Research this 
+browse.set_handle_equiv(True)
 
 
-# Dig into what this line does.
+# Deals with redirects
+#browse.set_handle_redirect(True)
+
+#handles refers
+#browse.set_handle_referer(True)
+
+#ignores robots.txt
+browse.set_handle_robots(False)
+
+
+# 
 # Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+# browse.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 
 # user-agent  - Presently Linux. Need to figure out how to get the one for the user's browser
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+browse.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 # Accesses Google
-r = br.open('http://google.com')
-html = r.read()
+pg = browse.open('http://google.com')
+html = pg.read()
 
 # Runs a search using the string "weekend codes"
-br.form['q']='weekend codes'
-br.submit()
+browse.form['q']='weekend codes'
+browse.submit()
 
-searchResult = br.response().read()
+searchResult=browse.response().read()
 
 
